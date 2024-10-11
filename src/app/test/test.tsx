008c46ab2eb2d99be221c9f282b25858ac97b816
Alex <alex.luxfero@gmail.com>
2024-10-11 23:42:51 +0500

    first commit

---
+++ src/app/test/test.tsx
@@ -0,0 +1,18 @@
+"use client"
+import {useBackButton} from "@telegram-apps/sdk-react";
+import {useEffect} from "react";
+
+export default function Test() {
+    const bb = useBackButton();
+
+    useEffect(() => {
+        bb.show();
+        return () => {
+            bb.hide();
+        }
+    }, [bb]);
+
+    return (
+        <div><h1>Test</h1></div>
+    )
+}
