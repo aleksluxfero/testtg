a3458d7d4ec18e8b60f8ce0987db5d9160e8e4b4
Alex <alex.luxfero@gmail.com>
2024-10-11 23:56:17 +0500

    fix

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,27 @@
 "use client"
 import {useBackButton} from "@telegram-apps/sdk-react";
 import {useEffect} from "react";
+import {useRouter} from "next/navigation";
 
 export default function Page() {
     const bb = useBackButton();
+    const router = useRouter();
 
     useEffect(() => {
         bb.show();
+        if(bb.isVisible) {
+            console.log("Кнопка видна");
+        }
+        const handleButtonClick = () => {
+            console.log("click");
+            router.push("/");
+        }
+        bb.on("click", handleButtonClick)
         return () => {
+            bb.off("click", handleButtonClick)
             bb.hide();
         }
-    }, [bb]);
+    }, [bb, router]);
 
     return (
         <div><h1>Test</h1></div>
